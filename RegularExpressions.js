0d178e373d9088372e72d789dd3b2e2732d7e0e1
Oleksandra-DT <dranishcheva@gmail.com>
2023-11-01 18:04:19 +0100

    regular_expressions

---
+++ RegularExpressions.js
@@ -0,0 +1,36 @@
+//Без А:
+/*
+/.{6,}[^aаAА]/
+
+*/
+
+// Параноя
+/*
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+var arr1 = [];
+for (const arrKey in arr) {
+    var user = arr[arrKey];
+    var re = /^([a-zA-Z0-9])\.+|([a-zA-Z0-9]+\.[a-zA-Z0-9]+)@((gmail\.com)|(yahoo\.com))$/;
+    if (re.test(user.email) === true){
+        arr1.push(user.email)
+    }
+}
+console.log(arr1)
+*/
+
